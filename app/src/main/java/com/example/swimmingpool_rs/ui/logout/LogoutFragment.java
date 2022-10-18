ed0278667a8fd95a081f032a168eda9f593da885
CrazyJoey <103395515+CrazyJoey4@users.noreply.github.com>
2022-10-18 20:42:11 +0800

    Update nav menu

---
+++ app/src/main/java/com/example/swimmingpool_rs/ui/logout/LogoutFragment.java
@@ -0,0 +1,33 @@
+package com.example.swimmingpool_rs.ui.logout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.swimmingpool_rs.databinding.FragmentLoginBinding;
+
+public class LogoutFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
