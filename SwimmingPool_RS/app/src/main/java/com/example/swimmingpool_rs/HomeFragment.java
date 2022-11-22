5d4d0dba9fd76cd773dbeb66a27e587dab8b6bf3
CrazyJoey <103395515+CrazyJoey4@users.noreply.github.com>
2022-11-22 20:23:52 +0800

    Slideshow

--- SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/HomeFragment.java
+++ SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/HomeFragment.java
@@ -1,18 +1,42 @@
 package com.example.swimmingpool_rs;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+    private ImageSlider mImageSlider;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+        mImageSlider = view.findViewById(R.id.slideshow);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.pooltype1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.pooltype2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.pooltype3, ScaleTypes.FIT));
+
+        mImageSlider.setImageList(slideModels, ScaleTypes.FIT);
     }
 }
