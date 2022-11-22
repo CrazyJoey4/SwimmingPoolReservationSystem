af8d6dcd630a45eb31a922e8c3d3d842588bca39
Teriyaki <103395356+PauThing@users.noreply.github.com>
2022-11-22 20:33:09 +0800

    update SDK

--- SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/LoginActivity.java
+++ SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/LoginActivity.java
@@ -1,14 +1,87 @@
 package com.example.swimmingpool_rs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://swimmingpoolreservation-f5910-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        final EditText email = findViewById(R.id.editTextTextEmailAddress);
+        final EditText password = findViewById(R.id.editTextTextPassword);
+
+        final Button login = findViewById(R.id.btnLogin);
+        final Button signup = findViewById(R.id.signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //get input data from user
+                final String emailTxt = email.getText().toString();
+                final String passwordTxt = password.getText().toString();
+
+                if(emailTxt.isEmpty() || passwordTxt.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter your email or password", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if(snapshot.hasChild(emailTxt)) {
+
+                                final String getPassword = snapshot.child(emailTxt).child("password").getValue(String.class);
+
+                                if(getPassword.equals(passwordTxt)) {
+                                    Toast.makeText(LoginActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+
+                                    //Lead to main activity
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(LoginActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else {
+                                Toast.makeText(LoginActivity.this, "Please make sure your email and password are correct", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
     }
 }
--- SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/SignupActivity.java
+++ SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/SignupActivity.java
@@ -1,13 +1,80 @@
 package com.example.swimmingpool_rs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://swimmingpoolreservation-f5910-default-rtdb.asia-southeast1.firebasedatabase.app/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        final EditText username = findViewById(R.id.editTextTextUsername);
+        final EditText fullname = findViewById(R.id.editTextTextFullname);
+        final EditText email = findViewById(R.id.editTextTextEmailAddress);
+        final EditText password = findViewById(R.id.editTextTextPassword);
+
+        final Button signup = findViewById(R.id.btnSignup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //get input data from user
+                final String usernameTxt = username.getText().toString();
+                final String fullnameTxt = fullname.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String passwordTxt = password.getText().toString();
+
+                if(usernameTxt.isEmpty() || fullnameTxt.isEmpty() || emailTxt.isEmpty() || passwordTxt.isEmpty()) {
+                    Toast.makeText(SignupActivity.this, "Please enter your details", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    databaseReference.child("users").addListenerForSingleValueEvent((new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            //check if the email is not registered
+                            if(snapshot.hasChild(emailTxt)) {
+                                Toast.makeText(SignupActivity.this, "This email already registered before", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+
+                                //send data to firebase
+                                //use email as unique identity
+                                databaseReference.child("users").child(emailTxt).child("username").setValue(usernameTxt);
+                                databaseReference.child("users").child(emailTxt).child("fullname").setValue(fullnameTxt);
+                                databaseReference.child("users").child(emailTxt).child("email").setValue(emailTxt);
+                                databaseReference.child("users").child(emailTxt).child("password").setValue(passwordTxt);
+
+                                //show a success message
+                                Toast.makeText(SignupActivity.this, "Sign Up successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    }));
+                }
+            }
+        });
     }
 }
