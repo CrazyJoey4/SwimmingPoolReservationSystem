d845270b3dfab2595232e99110eb4a4748484f2c
CrazyJoey <103395515+CrazyJoey4@users.noreply.github.com>
2022-11-23 15:46:57 +0800

    update

--- SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/SignupActivity.java
+++ SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/SignupActivity.java
@@ -1,6 +1,5 @@
 package com.example.swimmingpool_rs;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,11 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
 
