11388f83d624754f6f593992f12efce12232f595
Teriyaki <103395356+PauThing@users.noreply.github.com>
2022-11-23 15:57:13 +0800

    update booking and login

--- SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/BookingFragment.java
+++ SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/BookingFragment.java
@@ -4,15 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class BookingFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_booking, container, false);
+    }
+
+    DatePicker datePicker;
+    Button btnConfirm;
+    TextView dateShow;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        dateShow = view.findViewById(R.id.dateShow);
+        datePicker = view.findViewById(R.id.bookDatePicker);
+        btnConfirm = view.findViewById(R.id.btnConfirm);
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateShow.setText("Date: " + datePicker.getDayOfMonth() +"/"+ (datePicker.getMonth() + 1) + "/" + datePicker.getYear());
+            }
+        });
     }
 }
--- SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/LoginActivity.java
+++ SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/LoginActivity.java
@@ -2,10 +2,7 @@ package com.example.swimmingpool_rs;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,77 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 public class LoginActivity extends AppCompatActivity {
-
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference databaseReference = database.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        final EditText email = findViewById(R.id.editTextTextEmailAddress);
-        final EditText password = findViewById(R.id.editTextTextPassword);
 
-        final Button login = findViewById(R.id.btnLogin);
-        final Button signup = findViewById(R.id.signup);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //get input data from user
-                final String emailTxt = email.getText().toString();
-                final String passwordTxt = password.getText().toString();
-
-                if(emailTxt.isEmpty() || passwordTxt.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Please enter your email or password", Toast.LENGTH_SHORT).show();
-                }
-                else{
-
-                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                            if(snapshot.hasChild(emailTxt)) {
-
-                                final String getPassword = snapshot.child(emailTxt).child("password").getValue(String.class);
-
-                                if(getPassword.equals(passwordTxt)) {
-                                    Toast.makeText(LoginActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
-
-                                    //Lead to main activity
-                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-
-                                }
-                                else {
-                                    Toast.makeText(LoginActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                            else {
-                                Toast.makeText(LoginActivity.this, "Please make sure your email and password are correct", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
-                }
-            }
-        });
-
-        signup.setOnClickListener(view -> {
-            Intent intent = new Intent(this, SignupActivity.class);
-            startActivity(intent);
-        });
     }
 }
