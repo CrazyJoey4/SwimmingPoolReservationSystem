778247dfbf6b7479b9e2317566616667af1e1d7d
CrazyJoey <103395515+CrazyJoey4@users.noreply.github.com>
2022-11-24 11:14:11 +0800

    format

--- SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/BookSummaryFragment.java
+++ SwimmingPool_RS/app/src/main/java/com/example/swimmingpool_rs/BookSummaryFragment.java
@@ -21,50 +21,18 @@ public class BookSummaryFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_booking, container, false);
+        return inflater.inflate(R.layout.fragment_booksummary, container, false);
     }
 
     DatePicker datePicker;
     Button btnConfirm;
     TextView dateShow;
-    Button btnSlot1, btnSlot2, btnSlot3, btnSlot4, btnSlot5, btnSlot6;
-    Button btnProceed;
-    TextView timeShow;
-    NumberPicker paxNum;
-    TextView paxShow;
-    RadioGroup radioGroup;
-    RadioButton pool1;
-    RadioButton pool2;
-    RadioButton pool3;
-    TextView poolShow;
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        dateShow = view.findViewById(R.id.dateShow);
-        datePicker = view.findViewById(R.id.bookDatePicker);
         btnConfirm = view.findViewById(R.id.btnConfirm);
-        btnSlot1 = view.findViewById(R.id.btnSlot1);
-        btnSlot2 = view.findViewById(R.id.btnSlot2);
-        btnSlot3 = view.findViewById(R.id.btnSlot3);
-        btnSlot4 = view.findViewById(R.id.btnSlot4);
-        btnSlot5 = view.findViewById(R.id.btnSlot5);
-        btnSlot6 = view.findViewById(R.id.btnSlot6);
-        timeShow = view.findViewById(R.id.timeShow);
-        btnProceed = view.findViewById(R.id.btnProceed);
-
-        paxNum = view.findViewById(R.id.paxNumber);
-        paxNum.setMaxValue(30);
-        paxNum.setMinValue(1);
-        paxShow = view.findViewById(R.id.paxShow);
-
-        radioGroup = view.findViewById(R.id.radioGroup);
-        pool1 = view.findViewById(R.id.poolT1);
-        pool2 = view.findViewById(R.id.poolT2);
-        pool3 = view.findViewById(R.id.poolT3);
-        poolShow = view.findViewById(R.id.poolShow);
 
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,71 +41,71 @@ public class BookSummaryFragment extends Fragment {
             }
         });
 
-        btnSlot1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timeShow.setText("Time Slot: " + btnSlot1.getText());
-            }
-        });
-
-        btnSlot2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timeShow.setText("Time Slot: " + btnSlot2.getText());
-            }
-        });
-
-        btnSlot3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timeShow.setText("Time Slot: " + btnSlot3.getText());
-            }
-        });
-
-        btnSlot4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timeShow.setText("Time Slot: " + btnSlot4.getText());
-            }
-        });
-
-        btnSlot5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timeShow.setText("Time Slot: " + btnSlot5.getText());
-            }
-        });
-
-        btnSlot6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timeShow.setText("Time Slot: " + btnSlot6.getText());
-            }
-        });
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch(checkedId){
-                    case R.id.poolT1:
-                        poolShow.setText("Pool Type: " + pool1.getText());
-                        break;
-                    case R.id.poolT2:
-                        poolShow.setText("Pool Type: " + pool2.getText());
-                        break;
-                    case R.id.poolT3:
-                        poolShow.setText("Pool Type: " + pool3.getText());
-                        break;
-                }
-            }
-        });
-
-        paxNum.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int i, int g) {
-                int valuePick = paxNum.getValue();
-                paxShow.setText("Pax No.: " + valuePick);
-            }
-        });
+//        btnSlot1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                timeShow.setText("Time Slot: " + btnSlot1.getText());
+//            }
+//        });
+//
+//        btnSlot2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                timeShow.setText("Time Slot: " + btnSlot2.getText());
+//            }
+//        });
+//
+//        btnSlot3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                timeShow.setText("Time Slot: " + btnSlot3.getText());
+//            }
+//        });
+//
+//        btnSlot4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                timeShow.setText("Time Slot: " + btnSlot4.getText());
+//            }
+//        });
+//
+//        btnSlot5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                timeShow.setText("Time Slot: " + btnSlot5.getText());
+//            }
+//        });
+//
+//        btnSlot6.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                timeShow.setText("Time Slot: " + btnSlot6.getText());
+//            }
+//        });
+//
+//        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                switch(checkedId){
+//                    case R.id.poolT1:
+//                        poolShow.setText("Pool Type: " + pool1.getText());
+//                        break;
+//                    case R.id.poolT2:
+//                        poolShow.setText("Pool Type: " + pool2.getText());
+//                        break;
+//                    case R.id.poolT3:
+//                        poolShow.setText("Pool Type: " + pool3.getText());
+//                        break;
+//                }
+//            }
+//        });
+//
+//        paxNum.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+//            @Override
+//            public void onValueChange(NumberPicker numberPicker, int i, int g) {
+//                int valuePick = paxNum.getValue();
+//                paxShow.setText("Pax No.: " + valuePick);
+//            }
+//        });
     }
 }
